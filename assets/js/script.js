570f1b34411585b934a4daa166ad781f45377e5b
Marissa Fandel <mfandel@alumni.nd.edu>
2022-02-22 22:17:55 -0700

    updates to JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,8 +4,10 @@
     // Displays humidity
     // Displays UV index
 
-// When user searches for a city, the city is added to localStorage
-// When user searches for a city, the city is added to list of Previous Searches as a button
+// When user searches for a city, the city is added to localStorage ✅ 
+// When user searches for a city, the city is added to list of Previous Searches as a button ✅ 
+
+// When the user refreshes the page, the Previous Searches are still there
 
 // When user clicks Clear Search History, the localStorage is cleared
 // When user clicks Clear Search History, the list of Previous Searches is cleared of all city buttons
@@ -22,7 +24,7 @@ var userInput = document.querySelector(".user-input");
 var searchBtn = document.querySelector(".search-button");
 var searchHistory = document.querySelector(".search-history");
 // Populate search history from localStorage or empty array
-var searchHistoryList = localStorage.getItem("searchHistory") || "[]";
+var searchHistoryList = JSON.parse(localStorage.getItem("city")) || [];
 var clearBtn = document.querySelector(".clear-history");
 
 var city = document.querySelector(".city-name");
@@ -33,3 +35,28 @@ var uvIndex = document.querySelector(".uv-index");
 
 var forcast = document.querySelector("forecast");
 
+
+// Event Listener & function to pull weather & add city buttons to list of Previous Searches
+searchBtn.addEventListener("click",displayWeather)
+function displayWeather(event) {
+    var newBtnValue = event.target.parentNode.children[0].value;
+    // console.log(newBtnValue);
+
+    searchHistoryList.push(newBtnValue);
+    localStorage.setItem("city", JSON.stringify(searchHistoryList));
+
+    for (var i=0; i<searchHistoryList.length; i++) {
+        var btnEl = searchHistoryList[i];
+        var newBtn = document.createElement("button");
+        newBtn.setAttribute("class", "btn btn-dark btn-lg m-3");
+        newBtn.textContent = newBtnValue;
+        searchHistory.append(newBtn);
+    };
+    
+};
+
+// Event Listener & function to clear search history
+clearBtn.addEventListener("click",clearHistory)
+function clearHistory() {
+
+};
