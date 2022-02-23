cfdbd7f9274a91209d3937f696655c111b846c7e
Marissa Fandel <mfandel@alumni.nd.edu>
2022-02-23 13:53:44 -0700

    updates to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
+// When user inputs a city, the latitude & longitude coordinates are saved to use in OneCall call ✅ 
+
+// OneCall call uses lat & lon to complete call to retrieve weather info
+
 // When the user types in a city & clicks search button, the current weather & 5-day forecast is displayed
     // Displays temp
     // Displays wind speed
@@ -17,8 +21,8 @@
 
 // Declare variables
 var APIkey = "&appid=7dca179715285dbff858d4faf04c2d05";
-var lat = "";
-var long = "";
+// var lat = "";
+// var long = "";
 var now = moment().format('MMMM Do YYYY, h:mm:ss a');
 // console.log(now);
 
@@ -37,7 +41,6 @@ var uvIndex = document.querySelector(".uv-index");
 
 var forcast = document.querySelector("forecast");
 
-
 // Function to display Previous Search History on page load
 function init() {
     for (var i=0; i<searchHistoryList.length; i++) {
@@ -51,6 +54,10 @@ function init() {
 // Call function to render previous search history
 init();
 
+// TESTING API CALLS:
+// console.log("http://api.openweathermap.org/geo/1.0/direct?q=Denver&appid=7dca179715285dbff858d4faf04c2d05") ✅ 
+// console.log("https://api.openweathermap.org/data/2.5/onecall?lat=39.7392364&lon=-104.9848623&appid=7dca179715285dbff858d4faf04c2d05") ✅ 
+
 // Event Listener & function to pull weather & add city buttons to list of Previous Searches
 searchBtn.addEventListener("click",renderWeather)
 function renderWeather() {
@@ -58,20 +65,39 @@ function renderWeather() {
     // console.log(newCity);
 
     // Display current weather info & 5-day forecast for user-inputted city
-    // Function with fetch to pull latitude & longitude coordinates to plug into OneCall & 5-day calls
+    // Function with fetch to pull latitude & longitude coordinates and then plug into OneCall API call
     function getCoords() {
+        
         var geoCodingCall = "http://api.openweathermap.org/geo/1.0/direct?q=" + newCity + APIkey;
+        console.log(geoCodingCall);
         fetch(geoCodingCall)
             .then(function(response) {
                 return response.json();
             })
             .then(function(data) {
-                console.log(data);
-                lat = data.lat;
-                long = data.lon;
+                // console.log(data);
+                var coords = {
+                lat: data[0].lat,
+                long: data[0].lon
+                }
+                // console.log(coords);
+                function getWeather() {
+                    var OneCallURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + coords.lat + "&lon=" + coords.long + APIkey;
+                    console.log(oneCallURL);
+                    
+                    fetch(oneCallURL)
+                    .then(function(response) {
+                        return response.json();
+                    })
+                    .then(function(data) {
+                        console.log(data);
+                    });
+                };
+                getWeather();
             });
-    }
+    };
     getCoords();
+    
     // Add new city button to search history
     searchHistoryList.push(newCity);
     localStorage.setItem("city", JSON.stringify(searchHistoryList));
